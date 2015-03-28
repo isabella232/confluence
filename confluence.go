16570f07f1ebe11df04cfed024e300813303c3b2
Mark Petrovic <mark.petrovic@xoom.com>
2015-03-28 09:42:05 -0700

    add a stub interface

--- confluence.go
+++ confluence.go
@@ -1,1 +1,16 @@
 package confluence
+
+type Confluence interface {
+	Foo()
+}
+
+type DefaultConfluence struct {
+	BaseURL  string
+	username string
+	password string
+	Confluence
+}
+
+func (d DefaultConfluence) Foo() {
+
+}
